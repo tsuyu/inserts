929b0dee371d25cd93fc06653c39209bff8ec8b0
tsuyu <mohamad.yusuf@hotmail.com>
2011-11-15 11:41:47 +0800

    Update inserts.php

--- inserts.php
+++ inserts.php
@@ -1,10 +1,27 @@
 <?php
 
-/**
-  * Enter description here ...
-  * @author tsuyu
-  *
-  */
+/*
+
+This file is part of inserts.
+
+    inserts is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    inserts is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with inserts.  If not, see <http://www.gnu.org/licenses/>.
+	
+	tsuyu
+	inserts.php
+        main class
+
+*/
   
 class inserts {
 
